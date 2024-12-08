c7ea9b41da09f6124cb83cf6cc52fa854021760c
Priyanku <priyankubiswas85@gmail.com>
2024-12-09 00:38:10 +0100

    sas

--- get_latest_version.sh
+++ get_latest_version.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 # Function to get the latest listed version of a NuGet package
 get_latest_version() {
   PACKAGE_ID=$1
   API_URL="https://api.nuget.org/v3/registration5-gz-semver2/${PACKAGE_ID}/index.json"
+  
+  echo $1
 
   # Fetch the JSON data from the API
   RESPONSE=$(curl -s $API_URL)
